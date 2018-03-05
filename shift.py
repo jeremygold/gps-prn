56b500a85072764fdada4ede18c2087d5a6e6d62
Jeremy Gold <jeremy.gold@ikegps.com>
2018-03-05 21:49:29 +1300

    Initial creation of PRN generation scripts

---
+++ shift.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+G1 = [1 for i in range(10)]
+
+def shift(register, feedback, output):
+    """GPS Shift Register
+    
+    :param list feedback: which positions to use as feedback (1 indexed)
+    :param list output: which positions are output (1 indexed)
+    :returns output of shift register:
+    
+    """
+    
+    # calculate output
+    out = [register[i-1] for i in output]
+    if len(out) > 1:
+        out = sum(out) % 2
+    else:
+        out = out[0]
+        
+    # modulo 2 add feedback
+    fb = sum([register[i-1] for i in feedback]) % 2
+    
+    # shift to the right
+    for i in reversed(range(len(register[1:]))):
+        register[i+1] = register[i]
+        
+    # put feedback in position 1
+    register[0] = fb
+    
+    return out
+
+# example:
+
+prn=''
+for i in range(100):
+    prn += str(shift(G1, [3,10], [10]))
+    # print (G1)
+
+print(prn)
